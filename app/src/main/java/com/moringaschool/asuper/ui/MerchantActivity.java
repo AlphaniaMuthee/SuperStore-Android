3e9ac3a2a7f9019ba1cf7d8f28cfe7883591873f
AlphaniaMuthee <walphania2@gmail.com>
2020-07-01 13:45:52 +0300

    -

---
+++ app/src/main/java/com/moringaschool/asuper/ui/MerchantActivity.java
@@ -0,0 +1,56 @@
+package com.moringaschool.asuper.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
+import com.moringaschool.asuper.R;
+
+public class MerchantActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView mImage;
+    NavigationView mNavigation;
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_merchant);
+
+        drawerLayout= findViewById(R.id.drawerLayout);
+        mImage= findViewById(R.id.menuImg);
+        mImage.setOnClickListener(this);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.drawablemenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        }
+        if (id == R.id.add) {
+            Intent intent = new Intent(MerchantActivity.this, AdminInviteActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+}
